34a1e0ee50f1aabcdcabbf37a8181ff77ca56e76
Miro <mr.mladenoff@gmail.com>
2019-02-25 11:38:08 -0800

    Adding New Item to Our Budget Controller

--- app.js
+++ app.js
@@ -23,32 +23,43 @@ const budgetCotroller = (function () {
             exp: 0,
             inc: 0
         }
-    }
+    };
+
+    return {
+
+ 
+            addItem: function (type, des, val) { 
+            var newItem, ID; 
+            // Create new ID
+            if(data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length -1].id + 1;
+            }else {
+                ID = 0;
+            }
+
+            //Create a new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            }else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new elemnt 
+            return newItem;
+         },
+
+            testing: function () {
+            console.log(data);
+            
+        }
+
+    };
 
 
-
-
-    
  })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -66,7 +77,7 @@ const UIController = (function() {
         inputDesciption: '.add__description',
         inputValue: '.add__value',
         inputBtn: '.add__btn'
-    }
+    };
 
     return {
         getInput: function () { 
@@ -80,7 +91,7 @@ const UIController = (function() {
          getDOMstrings: function() { 
              return DOMstrings;
           }
-    }
+    };
 
 
 })();
@@ -106,12 +117,13 @@ const controller = (function (budgetCtrl, UIctrl) {
 
 
     const ctrlAddItem = function () { 
-
+        var input, newItem;
         //1. Get the filed input data
-        var input = UIctrl.getInput();        
+        input = UIctrl.getInput();        
    
         //2. Add the item to the budget controller
-   
+        newItem = budgetCotroller.addItem(input.type, input.description, input.value);
+
         //3. Add the item to the UI
    
         // 4. Calculate Budget
